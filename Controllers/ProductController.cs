43ac08c5df8c530ea04b815d83ca2dd7d0ac2698
dzape <predrag.devtools@gmail.com>
2023-11-28 20:08:54 +0100

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductOrders.BL.Repository;
+using ProductOrders.Data.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProductOrders.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        // GET: api/<ProductController>
+        [HttpGet]
+        public IEnumerable<Product> GetAll()
+        {
+            return _productRepository.GetAllProducts();
+        }
+
+        // GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public Product Get(int id)
+        {
+            return _productRepository.GetProductById(id);
+        }
+
+        // POST api/<ProductController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ProductController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
